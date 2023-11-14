6ae286515920d435a57489a475b520a947136a4a
kyuhunjo <joe24@naver.com>
2023-11-14 15:51:42 +0900

    drawing

--- src/pages/ToolInterface/ToolInterface.jsx
+++ src/pages/ToolInterface/ToolInterface.jsx
@@ -1,23 +1,52 @@
-import React from 'react';
-import './ToolInterface.css'; // Assuming you have a CSS file for ToolInterface
+import React, { useRef, useEffect } from 'react';
+import './ToolInterface.css';
 
 const ToolInterface = () => {
+    const canvasRef = useRef(null);
+    let isDrawing = false;
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        // Add more canvas setup here if needed
+
+        const startDrawing = (event) => {
+            isDrawing = true;
+            
+            context.beginPath();
+            context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+        };
+
+        const draw = (event) => {
+            if (!isDrawing) return;
+            context.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+            context.stroke();
+        };
+
+        const stopDrawing = () => {
+            isDrawing = false;
+            context.closePath();
+        };
+
+        canvas.addEventListener('mousedown', startDrawing);
+        canvas.addEventListener('mousemove', draw);
+        canvas.addEventListener('mouseup', stopDrawing);
+        canvas.addEventListener('mouseout', stopDrawing);
+
+        return () => {
+            canvas.removeEventListener('mousedown', startDrawing);
+            canvas.removeEventListener('mousemove', draw);
+            canvas.removeEventListener('mouseup', stopDrawing);
+            canvas.removeEventListener('mouseout', stopDrawing);
+        };
+    }, []);
+
     return (
         <div className="tool-interface">
             <div className="canvas-container">
-                <canvas id="webtoonCanvas"></canvas>
+                <canvas ref={canvasRef} id="webtoonCanvas" width={800} height={600}></canvas>
                 <aside id="toolbar" className="toolbar">
-                    {/* Tool buttons */}
-                    <div className="tool-group">
-                        <button className="tool-button" id="pencilTool">Pencil</button>
-                        <button className="tool-button" id="eraserTool">Eraser</button>
-                        {/* Add more tool buttons as needed */}
-                    </div>
-                    <div className="tool-group">
-                        <button className="tool-button" id="undoButton">Undo</button>
-                        <button className="tool-button" id="redoButton">Redo</button>
-                        {/* Preset and template selection can go here */}
-                    </div>
+                    {/* Toolbar buttons */}
                 </aside>
             </div>
         </div>
